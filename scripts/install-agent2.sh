691641745874941bee363176d84f4b683239e205
servadmin <servadmin@ubuntuserv>
2022-02-20 12:32:31 -0500

    Upgrade to Zabbix 6.0.0

--- scripts/install-agent2.sh
+++ scripts/install-agent2.sh
@@ -12,7 +12,7 @@
 #
 
 # Zabbix Server URL
-zabbixurl="https://cdn.zabbix.com/zabbix/sources/stable/5.4/zabbix-5.4.9.tar.gz"
+zabbixurl="https://cdn.zabbix.com/zabbix/sources/stable/6.0/zabbix-6.0.0.tar.gz"
 
 # Just Zabbix server archive name
 zabbixarchive=$(basename "$zabbixurl")
