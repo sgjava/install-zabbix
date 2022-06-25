85cd2d233c910fc26afd5cd305b09fd6cc6d6a18
Steven Goldsmith <sgjava@gmail.com>
2022-06-25 11:16:56 -0400

    PHP 8 not supported

--- scripts/install.sh
+++ scripts/install.sh
@@ -166,7 +166,10 @@ _EOF_
 
 	# Install webserver
 	log "Installing Apache and PHP..."
-	sudo -E apt-get -y install fping apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-gd php-xml php-bcmath php-ldap mlocate >> $logfile 2>&1
+	# PHP 8 not supported, so we need to select 7.4
+	sudo -E add-apt-repository ppa:ondrej/php -y sudo -E apt update >> $logfile 2>&1
+	sudo -E apt update >> $logfile 2>&1
+	sudo -E apt-get -y install fping apache2 php7.4 libapache2-mod-php7.4 php7.4-cli php7.4-mysql php7.4-mbstring php7.4-gd php7.4-xml php7.4-bcmath php7.4-ldap mlocate >> $logfile 2>&1
 	sudo -E updatedb >> $logfile 2>&1
 	# Get php.ini file location
 	phpini=$(locate php.ini 2>&1 | head -n 1)
