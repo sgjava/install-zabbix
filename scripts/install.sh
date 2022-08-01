2fdb04be4230e953bcd1f1e0215299d5ce88b9ab
Steven Goldsmith <sgjava@gmail.com>
2022-08-01 16:36:21 -0400

    Updated Zabbix to 6.2.1

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,7 +23,7 @@ dbzabbix="zabbixZaq!2wsx"
 monzabbix="monzabbixZaq!2wsx"
 
 # Zabbix Server URL
-zabbixurl="https://cdn.zabbix.com/zabbix/sources/stable/6.2/zabbix-6.2.0.tar.gz"
+zabbixurl="https://cdn.zabbix.com/zabbix/sources/stable/6.2/zabbix-6.2.1.tar.gz"
 
 # Just Zabbix server archive name
 zabbixarchive=$(basename "$zabbixurl")
