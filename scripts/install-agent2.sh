589abd9527b1dbd505776a4c80afe8e515aef878
Steven P. Goldsmith <sgjava@gmail.com>
2025-10-13 19:43:45 -0400

    Update install-agent2.sh
    Added required deb for 7.4

--- scripts/install-agent2.sh
+++ scripts/install-agent2.sh
@@ -70,7 +70,7 @@ else
 	sudo -E apt update >> $logfile 2>&1
 	sudo -E groupadd zabbix >> $logfile 2>&1
 	sudo -E useradd -g zabbix -s /bin/bash zabbix >> $logfile 2>&1
-	sudo -E apt-get -y install build-essential pkg-config libpcre3-dev libz-dev golang-go >> $logfile 2>&1
+	sudo -E apt-get -y install build-essential pkg-config libpcre2-dev libpcre3-dev libz-dev golang-go >> $logfile 2>&1
 fi
 cd "${srcdir}/${filename}" >> $logfile 2>&1
 # Patch source to fix "plugins/proc/procfs_linux.go:248:6: constant 1099511627776 overflows int" on 32 bit systems
